635d3836f5018cce50bbae0a52e3eeff414ecab6
Tom Lodge <tlodge@dhcp-22-212.mrl.nottingham.ac.uk>
2012-09-06 11:26:56 +0100

    fixed SQLALchemy name issue

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = sqlAlchemy(app)
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
