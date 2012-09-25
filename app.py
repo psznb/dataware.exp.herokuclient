bc33aba313fd6623d6cacac60243c60cfa998978
Tom Lodge <tlodge@dhcp-22-227.mrl.nottingham.ac.uk>
2012-09-25 14:23:39 +0100

    dev update

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from models import *
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
+app.config.from_object('settings')
 init_db()
 
 CATALOG     = "http://datawarecatalog.appspot.com"
@@ -199,4 +200,5 @@ def execute():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+   
     app.run(debug=True,host='0.0.0.0', port=port)
