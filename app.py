e32a551d29b32f1e6fe63e05b4d54f48ed173ab4
Tom Lodge <tlodge@dhcp-22-219.mrl.nottingham.ac.uk>
2012-11-28 14:36:26 +0000

    fiddling with json response from resource

--- app.py
+++ app.py
@@ -287,25 +287,52 @@ def result(execution_id):
     
     success = True
     
+    print request.form
     response = request.form['response']
+    print response
     
-    data = json.loads(response.replace( '\r\n','\n' ), strict=False)
+    print json.loads(response.replace( '\r\n','\n' ), strict=False)
     
-    success = data['success']
+    #print response
     
-    try:
-        if (success):
-            execution_request = getExecutionRequest(execution_id)
-            if (not execution_request is None):
-                result = data['return']
-                addExecutionResponse(execution_id=execution_id, access_token=execution_request.access_token, result=json.dumps(result), received=int(time.time()))
-        else:
-            print "not doing anything at the mo!"
+    #try:
+    #    if (request.form['success'] == 'True'):
+    #        execution_request = getExecutionRequest(execution_id)
+            #result = json.loads(data.replace( '\r\n','\n' ), strict=False)
+    #        print "0----"
+    #        print request.form['return']
+    #        print "0.5----"
+    #        print "1----"
+    #        print str(request.form['return'])
+    #        print "2----"
+    #        print "and finnally %s" % json.dumps(request.form['return'])
+    #        print "-----"
+    #    else:
+    #        print "not doing anything at the mo!"
                         
-    except:
-        success = False
+    #except:
+    #    success = False
         
-    return json.dumps({'success':success})  
+    return json.dumps({'success':success}) 
+        
+    #print request.form
+    
+    #data = request.form['return']
+    
+    #
+    
+    #result = json.loads(data.replace( '\r\n','\n' ), strict=False)
+    
+    #values = result['return']
+    
+    #print "--------VALUES---------------"
+    #print json.dumps(values)
+    #print "-------------------------"
+    
+    #if not(execution_request is None):
+    #    addExecutionResponse(execution_id=execution_id, access_token=execution_request.access_token, result=json.dumps(values), received=int(time.time()))
+        
+    
     
 @app.route('/view/<execution_id>', methods=['POST'])
 def view(execution_id):
