e92c9c9259faa3e6c75a7cc3ddc40e48b1a7f9a6
Tom Lodge <tlodge@dhcp-22-239.mrl.nottingham.ac.uk>
2012-12-17 15:00:07 +0000

    adding longpoll support

--- app.py
+++ app.py
@@ -264,7 +264,7 @@ def token():
     um.trigger({    
         "type": "resource",
         "message": "a resource request has been considered",
-        "data": json.dumps({'a':'resource'});                     
+        "data": json.dumps({'a':'resource'})                    
     });
     
     #if successful, swap the auth code for the token proper with catalog
